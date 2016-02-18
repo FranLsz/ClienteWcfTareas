ae9bd4fa15de75ef3120b3a4a76f989731eef22d
FranLsz <franlsz95develop@gmail.com>
2016-02-18 12:24:15 +0100

    no message

---
+++ ClienteWcfTareas/Controllers/UsuariosSoapController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClienteWcfTareas.TareasService;
+
+namespace ClienteWcfTareas.Controllers
+{
+    public class UsuariosSoapController : Controller
+    {
+        // GET: UsuariosSoap
+        public ActionResult Index()
+        {
+            var cl = new ServicioUsuarioClient();
+            var users = cl.GetAllUsuario();
+
+            return View(users);
+        }
+
+        public ActionResult Alta()
+        {
+            return View(new UsuarioDTO());
+        }
+
+        [HttpPost]
+        public ActionResult Alta(UsuarioDTO model)
+        {
+            var cl = new ServicioUsuarioClient();
+            cl.AddUsuario(model);
+            return View(model);
+        }
+    }
+}
